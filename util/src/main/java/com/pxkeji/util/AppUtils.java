5364e6cef28e02679ca789578df0fbecf0255e39
Eastwood <leon_eastwood@126.com>
2018-08-27 16:55:01 +0800

    AppUtils

---
+++ util/src/main/java/com/pxkeji/util/AppUtils.java
@@ -0,0 +1,37 @@
+package com.pxkeji.util;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+public class AppUtils {
+    /**
+     * 获取版本号
+     * @return 当前应用的版本号
+     */
+    public static String getVersionName(Context context) {
+        try {
+            PackageManager manager = context.getPackageManager();
+            PackageInfo info = manager.getPackageInfo(context.getPackageName(), 0);
+            return info.versionName;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "1.0";
+        }
+    }
+
+    /**
+     * 获取版本号
+     * @return 当前应用的版本号
+     */
+    public static int getVersionCode(Context context) {
+        try {
+            PackageManager manager = context.getPackageManager();
+            PackageInfo info = manager.getPackageInfo(context.getPackageName(), 0);
+            return info.versionCode;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 1;
+        }
+    }
+}
