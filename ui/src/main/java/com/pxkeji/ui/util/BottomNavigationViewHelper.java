43fe9a014a9938149604d6298d1eedabc6fec314
Eastwood <leon_eastwood@126.com>
2018-12-05 13:48:02 +0800

    BottomNavigationView

---
+++ ui/src/main/java/com/pxkeji/ui/util/BottomNavigationViewHelper.java
@@ -0,0 +1,33 @@
+package com.pxkeji.ui.util;
+
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
+import android.util.Log;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by Administrator on 2018/1/10.
+ */
+
+public class BottomNavigationViewHelper {
+
+    public static void disableShiftMode(BottomNavigationView view) {
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
+        try {
+            menuView.setLabelVisibilityMode(1);
+            for (int i = 0; i < menuView.getChildCount(); i++) {
+                BottomNavigationItemView item = (BottomNavigationItemView) menuView.getChildAt(i);
+                //noinspection RestrictedApi
+                item.setShifting(false);
+
+                // set once again checked value, so view will be updated
+                //noinspection RestrictedApi
+                item.setChecked(item.getItemData().isChecked());
+            }
+        } catch (Exception e) {
+            Log.e("BNVHelper", "Unable to change value of shift mode", e);
+        }
+    }
+}
