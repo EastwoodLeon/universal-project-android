03a701f7dfdc785f3f413def6978dcc4e9f95500
Eastwood <leon_eastwood@126.com>
2019-01-10 08:27:57 +0800

    ScrollTextView

---
+++ ui/src/main/java/com/pxkeji/ui/view/ScrollTextView.java
@@ -0,0 +1,45 @@
+package com.pxkeji.ui.view;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatTextView;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+
+/**
+ * 类名：ScrollTextView
+ * 描述：自动滚动的TextView,跑马灯效果
+ * 创建人：Yun.Lei on 2017年6月21日
+ * 修改人：
+ * 修改时间：
+ * 修改备注：
+ */
+public class ScrollTextView extends AppCompatTextView {
+
+    public ScrollTextView(Context context) {
+        super(context);
+        initView();
+    }
+
+    public ScrollTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView();
+    }
+
+    public ScrollTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView();
+    }
+
+    private void initView() {
+        setEllipsize(TextUtils.TruncateAt.MARQUEE);
+        setMarqueeRepeatLimit(-1);
+        setSingleLine(true);
+    }
+
+
+
+    @Override
+    public boolean isFocused() {
+        return true;
+    }
+}
